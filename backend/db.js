9aa2971f49d3af6a68836fa2fed8394ee686c1ba
hariharan81k <hariharankannan36@gmail.com>
2026-01-19 15:43:14 +0530

    Changed the MySQL data to another laptop

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "server@hari18!",
+    password: "vpk0804",
     database: "student_portal"
 });
 
