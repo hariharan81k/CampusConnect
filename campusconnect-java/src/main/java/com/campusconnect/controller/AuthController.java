d02930b32285eeed80fb89346834024ed14d736c
hariharan81k <hariharankannan36@gmail.com>
2026-01-21 20:45:15 +0530

    modified the api/login in AuthController.java

--- campusconnect-java/src/main/java/com/campusconnect/controller/AuthController.java
+++ campusconnect-java/src/main/java/com/campusconnect/controller/AuthController.java
@@ -18,12 +18,18 @@ public class AuthController {
     private StudentRepository studentRepo;
 
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody Student req) {
+    public Map<String, Object> login(@RequestBody Map<String, String> req) {
+
+        System.out.println("Inside student login API");
+
+        String userid = req.get("userid");
+        String password = req.get("password");
 
         Map<String, Object> res = new HashMap<>();
-        Student s = studentRepo.findByUserid(req.getUserid()).orElse(null);
 
-        if (s != null && s.getPassword().equals(req.getPassword())) {
+        Student s = studentRepo.findByUserid(userid).orElse(null);
+
+        if (s != null && s.getPassword().equals(password)) {
             res.put("success", true);
             res.put("userid", s.getUserid());
             res.put("name", s.getName());
