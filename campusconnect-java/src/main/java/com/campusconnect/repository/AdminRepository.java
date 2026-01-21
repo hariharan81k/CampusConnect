cf182f58ecb89b3120496aff63a6b06a0510ed29
hariharan81k <hariharankannan36@gmail.com>
2026-01-21 12:45:18 +0530

    created new file Admin.java, amd AdminRepository.java

---
+++ campusconnect-java/src/main/java/com/campusconnect/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.campusconnect.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.campusconnect.model.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByUsername(String username);
+}
